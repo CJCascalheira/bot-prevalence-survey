1620c7d2cbcbe223a2a6e53f6cbb8139d334fb8a
Cory Cascalheira <corycaaz@gmail.com>
2023-04-20 13:21:19 -0500

    Prepare for qualitative fraud checking of data

--- src/qual_fraud_check.R
+++ src/qual_fraud_check.R
@@ -15,7 +15,7 @@ library(tidyverse)
 # Import data
 passed_bdts_04.20.23 <- read_csv("data/passed_bdts/passed_bdts_04.20.23.csv")
 
-# PREPARE DATA FRAMES -----------------------------------------------------
+# PREPARE DATA FRAMES - A -------------------------------------------------
 
 # Select the data and create variables
 qual_review_a <- passed_bdts_04.20.23 %>%
@@ -29,6 +29,20 @@ qual_review_a <- passed_bdts_04.20.23 %>%
   mutate(has_edu_email = if_else(has_edu_email > 0, 1, 0)) %>%
   # Name the qual review 
   mutate(qual_review = "A") %>%
-  # Add columns for raters to use
   select(ResponseId, qual_review, has_edu_email, no_bots_experience, starts_with("qual"))
 qual_review_a
+
+# Divide the data
+qual_review_a_no_bot_exp <- qual_review_a %>%
+  filter(no_bots_experience == 1) %>%
+  select(ResponseId, qual_review, has_edu_email, qual_imagine)
+qual_review_a_no_bot_exp
+
+qual_review_a_yes_bot_exp <- qual_review_a %>%
+  filter(no_bots_experience == 0) %>%
+  select(-no_bots_experience, -qual_imagine)
+qual_review_a_yes_bot_exp
+
+# Write to file
+write_csv(qual_review_a_no_bot_exp, "data/qual_fraud_check/not_coded/qual_review_a_no_bot_exp.csv")
+write_csv(qual_review_a_yes_bot_exp, "data/qual_fraud_check/not_coded/qual_review_a_yes_bot_exp.csv")
